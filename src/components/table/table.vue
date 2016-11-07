3db96262a3d4f1c2de26104cde23f7f1ef7133b2
cheney3w <406653843@qq.com>
2016-11-07 17:48:49 +0800

    表格组件可以自由调整宽度了

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -2,10 +2,10 @@
   <table class="table" :class="tableStyle">
     <tbody>
       <tr>
-        <th v-for="field in tdata.fields" :align="field.align" @click="reverse">{{ field.name }} <i v-if="field.align" class="fa" :class="iconTitle"></i></th>
+        <th @dragover.prevent="" v-for="(field, idx) in tdata.fields" :align="field.align" @click="reverse" :style="{flexBasis: field.width, flexGrow: field.width ? 0 : 1}">{{ field.name }} <i v-if="field.align" class="fa" :class="iconTitle"></i><span class="editable-border" draggable="true" @drag="drag" @dragstart="dragstart" @dragend="dragend" :data-idx="idx"></span></th>
       </tr>
       <tr v-for="row in tdata.rows">
-        <td v-for="rowData in row">{{ rowData }}</td>
+        <td v-for="(rowData, idx) in row" :style="{flexBasis: tdata.fields[idx].width, flexGrow: tdata.fields[idx].width ? 0 : 1}">{{ rowData }}</td>
       </tr>
     </tbody>
   </table>
@@ -25,7 +25,8 @@ export default {
   data () {
     return {
       tdata: {},
-      iconTitle: 'fa-sort-desc'
+      iconTitle: 'fa-sort-desc',
+      drag: {}
     }
   },
   methods: {
@@ -33,6 +34,23 @@ export default {
       if ($event.currentTarget.align) {
         this.iconTitle = this.iconTitle === 'fa-sort-desc' ? 'fa-sort-asc' : 'fa-sort-desc'
         this.tdata.rows.reverse()
+      }
+    },
+    dragstart ($event) {
+      if (!this.drag.flag) {
+        this.drag.flag = true
+        this.drag.w = window.getComputedStyle($event.target.parentElement).width
+        this.drag.x = $event.pageX
+      }
+    },
+    drag ($event) {
+      if (this.drag.flag) {
+        this.tdata.fields[$event.target.dataset.idx].width = parseInt(this.drag.w) + ($event.pageX - this.drag.x) + 'px'
+      }
+    },
+    dragend ($event) {
+      if (this.drag.flag) {
+        this.drag.flag = false
       }
     }
   },
@@ -46,6 +64,27 @@ export default {
 
 <style lang="scss">
 
+.table {
+
+  th {
+    position: relative;
+  }
+
+  .editable-border {
+    position: absolute;
+    right: -3px;
+    top: 0;
+    display: inline-block;
+    width: 5px;
+    height: 100%;
+    // background-color: #000;
+
+    &:hover {
+      cursor:col-resize;
+    }
+  }
+}
+
 .table.table-border {
 
   width: 100%;
@@ -56,6 +95,8 @@ export default {
 
   tr {
     border-bottom: 1px solid #ccc;
+    display: flex;
+    flex-flow: row nowrap;
 
     &:last-child {
       border-bottom: none;
@@ -70,15 +111,16 @@ export default {
     border-right: 1px solid #ccc;
     padding: 10px;
     cursor: pointer;
-
-    &:hover {
-      background-color: #000;
-      color: #fff;
-    }
+    flex: 1 1 0;
 
     &:last-child {
       border-right: none;
     }
+  }
+
+  td:hover {
+    background-color: #000;
+    color: #fff;
   }
 }
 
