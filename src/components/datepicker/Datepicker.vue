7031382dbb066f47382ba19d75c4a5a2c6a9b9e6
cheney3w <406653843@qq.com>
2016-11-11 16:25:52 +0800

    optimize dp

--- src/components/datepicker/Datepicker.vue
+++ src/components/datepicker/Datepicker.vue
@@ -1,17 +1,29 @@
 <template lang="html">
-  <div class="dp-panel">
-    <div class="dp-header">
-      <i class="fa fa-angle-double-left" @click.stop="switchYear(-1)"></i>
-      <i class="fa fa-angle-left" @click.stop="switchMonth(-1)"></i>
-      <span class="dp-header-caption" @click.stop="returnToToday">{{`${year}年${month}月`}}</span>
-      <i class="fa fa-angle-right" @click.stop="switchMonth(1)"></i>
-      <i class="fa fa-angle-double-right" @click.stop="switchYear(1)"></i>
-    </div>
-    <div class="dp-main">
-      <div class="dp-main-caption">
-        <span class="dp-grid dp-grid-caption" v-for="day in weekdays" :key="day">{{day}}</span>
-        <div v-for="(dateArr, idx) in dateArrs" :key="idx">
-          <span class="dp-grid dp-grid-date" v-for="dateObj in dateArr" :key="dateObj.date" :class="dateObj.className">{{dateObj.dateDay}}</span>
+  <div>
+    <div class="dp-output">{{showTime}}</div>
+    <div class="dp-panel">
+      <div class="dp-header">
+        <i class="fa fa-angle-double-left" @click.stop="switchYear(-1)"></i>
+        <i class="fa fa-angle-left" @click.stop="switchMonth(-1)"></i>
+        <span class="dp-header-caption" @click.stop="returnToToday">{{`${year}年${month}月`}}</span>
+        <i class="fa fa-angle-right" @click.stop="switchMonth(1)"></i>
+        <i class="fa fa-angle-double-right" @click.stop="switchYear(1)"></i>
+      </div>
+      <div class="dp-main">
+        <div class="dp-main-caption">
+          <span class="dp-grid dp-grid-caption" v-for="day in weekdays" :key="day">{{day}}</span>
+          <div v-for="(dateArr, idx) in dateArrs" :key="idx">
+            <span class="dp-grid dp-grid-date" v-for="dateObj in dateArr" :key="dateObj.date" :data-date="dateObj.date" :class="dateObj.className" @click.stop="selectDate">{{dateObj.dateDay}}</span>
+          </div>
+        </div>
+      </div>
+      <div class="dp-jump" @click.self="showQJPanel">
+        快速跳转
+        <div class="dp-jump-panel" @keyup.enter.capture="quickJumpTo" v-show="quickJump.showPanel">
+          <input v-model.number="quickJump.year"/>
+          年
+          <input v-model.number="quickJump.month"/>
+          月
         </div>
       </div>
     </div>
@@ -19,8 +31,6 @@
 </template>
 
 <script>
-const Vue = require('vue')
-
 export default {
   name: 'datepicker',
   data () {
@@ -30,7 +40,13 @@ export default {
         [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
         [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
       ],
-      dateArrs: []
+      dateArrs: [],
+      showTime: '请选择日期',
+      quickJump: {
+        year: '',
+        month: '',
+        showPanel: false
+      }
     }
   },
   computed: {
@@ -43,6 +59,36 @@ export default {
     }
   },
   methods: {
+    showQJPanel () {
+      this.quickJump.showPanel = !this.quickJump.showPanel
+    },
+    quickJumpTo () {
+      let year = this.quickJump.year
+      let month = this.quickJump.month
+      if (!year && !month) {
+        return
+      } else if (!year) {
+        this.month = this.quickJump.month
+        this.execute(new Date(year, this.month, 1))
+        this.quickJump.month = ''
+        this.showQJPanel()
+      } else if (!month) {
+        this.year = this.quickJump.year
+        this.execute(new Date(this.year, month, 1))
+        this.quickJump.year = ''
+        this.showQJPanel()
+      } else {
+        this.execute(new Date(year, month, 1))
+        this.quickJump.year = ''
+        this.quickJump.month = ''
+        this.showQJPanel()
+      }
+    },
+    selectDate (event) {
+      let date = new Date(event.target.dataset.date)
+
+      this.showTime = date.getFullYear() + '年' + date.getMonth() + '月' + date.getDate() + '日'
+    },
     returnToToday () {
       this.execute(new Date())
     },
@@ -125,10 +171,10 @@ export default {
       const firstDay = this.getDate(firstDayOfMonth, -firstDayOfMonth.getDay())
       const rowAmount = this.computeRows(today.day, today.mongth, today.year)
       for (let i = 0, oft = 0; i < rowAmount; i++) {
-        Vue.set(this.dateArrs, i, [])
+        this.$set(this.dateArrs, i, [])
         for (let j = 0; j < 7; j++) {
           // this.dateArrs[i][j] = this.getDate(firstDay, oft)
-          Vue.set(this.dateArrs[i], j, this.createDateObj(this.getDate(firstDay, oft), date))
+          this.$set(this.dateArrs[i], j, this.createDateObj(this.getDate(firstDay, oft), date))
           oft++
         }
       }
@@ -165,6 +211,7 @@ export default {
       display: inline-block;
       width: 30px;
       height: 20px;
+      cursor: pointer;
 
       &.date-today {
         color: #f00;
